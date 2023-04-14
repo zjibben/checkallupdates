fe3e5ec477b1b87cada2c0ec76afcff3855f8b50
Zach Jibben <zjibben@gmail.com>
2023-04-14 17:10:42 -0600

    Tidy up

--- checkallupdates.py
+++ checkallupdates.py
@@ -17,7 +17,7 @@ def isNewVersion(update):
     return update[2][:update[2].rfind('-')] != update[3][:update[3].rfind('-')]
 
 # return a table generated from the given header and data
-def table (header, alignment, data):
+def table(header, alignment, data):
     t = texttable.Texttable(max_width=1000) # let the terminal wrap lines, if necessary
     t.set_deco(texttable.Texttable.HEADER)
     t.set_cols_align(alignment)
@@ -27,10 +27,10 @@ def table (header, alignment, data):
     return t.draw()
 
 # return a list with certain elements removed
-def removeOccurances (the_list, forbidden): return [x for x in the_list if x not in forbidden]
+def removeOccurances(the_list, forbidden): return [x for x in the_list if x not in forbidden]
 
 # find the repositories associated with a list of packages
-def packageRepositories (pkg):
+def packageRepositories(pkg):
     # get all package info associated with every package
     pkg_info  = subprocess.Popen(["pacman", "-Si"] + pkg, stdout=subprocess.PIPE).stdout.read()
     pkg_Qinfo = subprocess.Popen(["pacman", "-Qi"] + pkg, stdout=subprocess.PIPE).stdout.read()
@@ -45,50 +45,52 @@ def packageRepositories (pkg):
 
     return pkg_repo
 
-# main body
-package_update = []
+if __name__=="__main__":
+    package_update = []
 
-# look for updates in the regular repos
-repo_update_text = subprocess.Popen("checkupdates", stdout=subprocess.PIPE).stdout.read().decode()
-for line in repo_update_text.splitlines():
-    pkgname, pkgverold, _, pkgvernew = line.split()
-    package_update.append([None, pkgname, pkgverold, pkgvernew])
+    # look for updates in the regular repos
+    repo_update_text = subprocess.Popen("checkupdates", stdout=subprocess.PIPE) \
+                                 .stdout.read().decode()
+    for line in repo_update_text.splitlines():
+        pkgname, pkgverold, _, pkgvernew = line.split()
+        package_update.append([None, pkgname, pkgverold, pkgvernew])
 
-# figure out which repo every package belongs to
-# do this for all the packages at once, rather than in the loop above,
-# because many calls to pacman ends up being much slower than one big
-# call that grabs the info for all packages being updated
-pkg_repo = packageRepositories([update[1] for update in package_update])
-for i in range(len(package_update)):
-    package_update[i][0] = pkg_repo[i]
-    
-# look for updates in the AUR
-# COULD SWITCH TO $ yay -Qu here? Likely still want checkupdates above
-# to avoid updating the sync database (which requires root).
-aur_update_text = subprocess.Popen(["yay", "-Qmu"], stdout=subprocess.PIPE).stdout.read().decode()
-for line in aur_update_text.splitlines():
-    pkgname, pkgverold, _, pkgvernew = line.split()
-    # I don't know how to assess upgrade/download size for AUR packages
-    # leaving as None for now
-    package_update.append(["aur", pkgname, pkgverold, pkgvernew])
-    
-# sort the list first by repo (core, extra, community, multilib, aur), then by package name
-sort_order = {"core": 0, "extra": 1, "community": 2, "multilib": 3, "aur": 4}
-package_update.sort(key=lambda x: sort_order[x[0]])
+    # figure out which repo every package belongs to
+    # do this for all the packages at once, rather than in the loop above,
+    # because many calls to pacman ends up being much slower than one big
+    # call that grabs the info for all packages being updated
+    pkg_repo = packageRepositories([update[1] for update in package_update])
+    for i in range(len(package_update)):
+        package_update[i][0] = pkg_repo[i]
 
-# split the list into new version updates and new release updates
-new_version = []; new_release = []
-for update in package_update:
-    if isNewVersion(update): new_version.append(update)
-    else:                    new_release.append(update)
+    # look for updates in the AUR
+    # COULD SWITCH TO $ yay -Qu here? Likely still want checkupdates above
+    # to avoid updating the sync database (which requires root).
+    aur_update_text = subprocess.Popen(["yay", "-Qmu"], stdout=subprocess.PIPE) \
+                                .stdout.read().decode()
+    for line in aur_update_text.splitlines():
+        pkgname, pkgverold, _, pkgvernew = line.split()
+        # I don't know how to assess upgrade/download size for AUR packages
+        # leaving as None for now
+        package_update.append(["aur", pkgname, pkgverold, pkgvernew])
 
-# print the data
-print("Software upgrade (new version)\n",
-      table(["Repository", "Package", "Old Version", "New Version"],
-            ["l", "l", "l", "l"],
-            new_version))
-print()
-print("Package upgrade only (new release)\n",
-      table(["Repository", "Package", "Old Version", "New Version"],
-            ["l", "l", "l", "l"],
-            new_release))
+    # sort the list first by repo (core, extra, community, multilib, aur), then by package name
+    sort_order = {"core": 0, "extra": 1, "community": 2, "multilib": 3, "aur": 4}
+    package_update.sort(key=lambda x: sort_order[x[0]])
+
+    # split the list into new version updates and new release updates
+    new_version = []; new_release = []
+    for update in package_update:
+        if isNewVersion(update): new_version.append(update)
+        else:                    new_release.append(update)
+
+    # print the data
+    print("Software upgrade (new version)\n",
+          table(["Repository", "Package", "Old Version", "New Version"],
+                ["l", "l", "l", "l"],
+                new_version))
+    print()
+    print("Package upgrade only (new release)\n",
+          table(["Repository", "Package", "Old Version", "New Version"],
+                ["l", "l", "l", "l"],
+                new_release))
